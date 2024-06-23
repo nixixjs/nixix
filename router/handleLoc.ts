9714ee9124b7e94c13a251109993199536a0dfe0
michTheBrandofficial <120549491+michTheBrandofficial@users.noreply.github.com>
2024-06-23 20:17:15 +0100

    [fix] some updates to loader

--- router/handleLoc.ts
+++ router/handleLoc.ts
@@ -5,38 +5,39 @@ import { isFunction } from "../shared";
 import { LoaderHandler, callLoader } from "./callLoader";
 
 export function handleLocation() {
-  const {
-    $$__routeStore: { provider, routeMatch },
-  } = nixixStore as Required<typeof nixixStore>;
-  switchRoutes({ provider, routeMatch });
-  callLoader(routeMatch!)
+	const {
+		$$__routeStore: { provider, routeMatch },
+	} = nixixStore as Required<typeof nixixStore>;
+	switchRoutes({ provider, routeMatch });
 }
 
 export function switchRoutes({
-  provider,
-  routeMatch,
+	provider,
+	routeMatch,
 }: Required<typeof nixixStore>["$$__routeStore"]) {
-  const route = routeMatch!.route;
-  const element = route.element;
-  switch (element) {
-    case null:
-    case undefined:
-      break;
-    default:
-      nixixStore.$$__routeStore!.currentRoute! = route;
-      let routePage: any;
-      if (isFunction(element)) {
-        routePage = buildComponent(element, {}, []);
-        route.element = routePage;
-      } else routePage = element;
-      provider?.replace(createFragment(routePage));
-      const [loaderState, setLoaderState] =
-        LoaderHandler.getLoaderState(route.path!)! || [];
-      setLoaderState?.({ ...loaderState, loading: true });
-      break;
-  }
+	const route = routeMatch!.route;
+	const element = route.element;
+	switch (element) {
+		case null:
+		case undefined:
+			break;
+		default:
+			if (nixixStore.$$__routeStore?.currentRoute === route) return;
+			nixixStore.$$__routeStore!.currentRoute! = route;
+			let routePage: any;
+			if (isFunction(element)) {
+				routePage = buildComponent(element, {}, []);
+				route.element = routePage;
+			} else routePage = element;
+			provider?.replace(createFragment(routePage));
+			const [loaderState, setLoaderState] =
+				LoaderHandler.getLoaderState(route.path!)! || [];
+			setLoaderState?.({ ...loaderState, loading: true });
+			callLoader(routeMatch!);
+			break;
+	}
 }
 
 export function changeTitle(title: string) {
-  document.title = title;
+	document.title = title;
 }
